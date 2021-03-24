f06ba1b5b47afe292dc3d5057d3f5ca767213966
EderIRE <71097226+EderIRE@users.noreply.github.com>
2021-03-24 18:41:46 +0000

    loading animation added  cardgames  section

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const links = document.querySelectorAll('.secIcon');
+const loader = document.querySelector('.loader');
 
 function show() {
   mainMenu.style.display = 'flex';
@@ -12,6 +13,11 @@ function close() {
   mainMenu.style.top = '-200%';
 }
 
+function loading() {
+  loader.parentElement.removeChild(loader);
+}
+
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
+loader.addEventListener('load', loading);
 links.forEach((icons) => icons.addEventListener('click', close));
